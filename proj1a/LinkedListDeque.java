200bc6f99f555c20cbfd0d5999584637dce175e0
dfgedc <632035098@qq.com>
2020-12-18 18:52:21 +0800

    proj1a complete1

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,23 +1,116 @@
-public class LinkedListDeque<T> {
-    public void addFirst(T item){
+public class LinkedListDeque<T> implements Deque<T>{
+    private int size ;
+    private Node first;
+    private class Node {
+        T item;
+        Node prev;
+        Node next;
+
+        Node(T i, Node p, Node n) {
+            item = i;
+            prev = p;
+            next = n;
+        }
+    }
+
+    public LinkedListDeque(){
+        first = new Node(null,null,null);
+        first.prev = first;
+        first.next = first;
+        size = 0;
+    }
+    public T getRecursive(int index){
+        if (index >= size) {
+            return null;
+        }
+
+        return getRecursiveHelper(first.next, index);
 
     }
-    public void addLast(T item){
+    /** Helper method for getRecursive */
+    private T getRecursiveHelper(Node currentNode, int index) {
+        if (index == 0) {
+            return currentNode.item;
+        }
 
+        return getRecursiveHelper(currentNode.next, index - 1);
     }
-    public boolean idEmpty(){
-
+    @Override
+    public int size() {
+        return size(first);
     }
-    public int size(){
-
+    private int size(Node node){
+        if(node.next==null)
+            return  1;
+        return 1+size(node.next);
     }
-    public void printDeque(){
-
+    @Override
+    public boolean isEmpty() {
+        if(first.next==first&&first.prev == first){
+            return true;
+        }
+        return false;
     }
-    public T removeFirst(){
 
+    @Override
+    public void addLast(T item) {
+        first.prev = new Node(item, first.prev, first);
+        first.prev.prev.next = first.prev;
+        size++;
     }
-    public T get(int index){
 
+    @Override
+    public void addFirst(T item) {
+          first.prev = new Node(item,first.prev,first);
+          first.prev.prev.next = first.prev;
+          size+=1;
+    }
+
+    @Override
+    public void printDeque() {
+            if(first.item==null) return;
+            while(first.next!=null){
+                System.out.print(first.item);
+                first = first.next;
+            }
+    }
+
+    @Override
+    public T removeFirst() {
+        if(first.next == first){
+            return null;
+        }
+        T removeItem = first.next.item;
+        first.next = first.next.next;
+        first.next.prev = first;
+        size--;
+        return removeItem;
+    }
+
+    @Override
+    public T removeLast() {
+        if(first.prev == first){
+            return null;
+
+        }
+        T removeItem = first.prev.item;
+        first.prev = first.prev.prev;
+        first.prev.next = first;
+        size--;
+        return removeItem;
+    }
+
+    @Override
+    public T get(int index) {
+     //   return null;
+        if(index>=size){
+            return null;
+        }
+        Node currentNode = first.next;
+        while (index!=0){
+            currentNode = currentNode.next;
+            index--;
+        }
+        return currentNode.item;
     }
 }
