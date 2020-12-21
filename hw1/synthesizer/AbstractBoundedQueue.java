6f89a8a71075f8381e778768a0318fbe6c6056f5
dfgedc <632035098@qq.com>
2020-12-21 17:08:34 +0800

    hw1 complete1

---
+++ hw1/synthesizer/AbstractBoundedQueue.java
@@ -0,0 +1,37 @@
+package synthesizer;
+
+public abstract class AbstractBoundedQueue<T> implements  BoundedQueue<T> {
+    protected int fillCount;
+    protected int capacity;
+ //   protected T[] data;
+/*
+    public AbstractBoundedQueue(int capacity) {
+        data = (T[]) new Object[capacity];
+    }
+
+    public AbstractBoundedQueue() {
+        data = (T[]) new Object[capacity];
+    }
+*/
+
+    public int capacity(){
+        return capacity;
+    }
+    public int fillCount(){
+        return fillCount;
+    }
+    public boolean isEmpty(){
+        if(fillCount==0)
+            return true;
+        return false;
+    }
+    public boolean isFull(){
+            if(fillCount==capacity)
+                return true;
+            else
+                return false;
+    }
+    public abstract T peek();
+    public abstract T dequeue();
+    public abstract void enqueue(T x);
+}
---
+++ hw1/synthesizer/GuitarString.java
@@ -0,0 +1,58 @@
+package synthesizer;// TODO: Make sure to make this class a part of the synthesizer package
+//package <package name>;
+
+//Make sure this class is public
+public class GuitarString {
+    /** Constants. Do not change. In case you're curious, the keyword final means
+     * the values cannot be changed at runtime. We'll discuss this and other topics
+     * in lecture on Friday. */
+    private static final int SR = 44100;      // Sampling Rate
+    private static final double DECAY = .996; // energy decay factor
+
+    /* Buffer for storing sound data. */
+    private synthesizer.BoundedQueue<Double> buffer;
+
+    /* Create a guitar string of the given frequency.  */
+    public GuitarString(double frequency) {
+        // TODO: Create a buffer with capacity = SR / frequency. You'll need to
+        //       cast the result of this divsion operation into an int. For better
+        //       accuracy, use the Math.round() function before casting.
+        //       Your buffer should be initially filled with zeros.
+        int capacity = (int) Math.round(SR / frequency);
+        buffer = new ArrayRingBuffer<>(capacity);
+        for (int i = 0; i < buffer.capacity(); i += 1) {
+            buffer.enqueue(0.0);
+        }
+    }
+
+
+    /**
+     * Plucks the guitar string by replacing the buffer with white noise.
+     */
+    public void pluck() {
+        for (double i : buffer) {
+            double r = Math.random() - 0.5;
+            buffer.dequeue();
+            buffer.enqueue(r);
+        }
+    }
+
+    /* Advance the simulation one time step by performing one iteration of
+     * the Karplus-Strong algorithm. 
+     */
+    public void tic() {
+        // TODO: Dequeue the front sample and enqueue a new sample that is
+        //       the average of the two multiplied by the DECAY factor.
+        //       Do not call StdAudio.play().
+        double front = buffer.dequeue();
+        double next = buffer.peek();
+        double plucked = DECAY * (front + next) / 2;
+        buffer.enqueue(plucked);
+    }
+
+    /* Return the double at the front of the buffer. */
+    public double sample() {
+        // TODO: Return the correct thing.
+        return buffer.peek();
+    }
+}
