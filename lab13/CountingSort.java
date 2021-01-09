1619aff2dd500f6956c115aa407490548c58f15f
dfgedc <632035098@qq.com>
2021-01-09 16:14:17 +0800

    lab13 complete

---
+++ lab13/CountingSort.java
@@ -0,0 +1,94 @@
+/**
+ * Class with 2 ways of doing Counting sort, one naive way and one "better" way
+ *
+ * @author Akhil Batra, Alexander Hwang
+ *
+ **/
+public class CountingSort {
+    /**
+     * Counting sort on the given int array. Returns a sorted version of the array.
+     * Does not touch original array (non-destructive method).
+     * DISCLAIMER: this method does not always work, find a case where it fails
+     *
+     * @param arr int array that will be sorted
+     * @return the sorted array
+     */
+    public static int[] naiveCountingSort(int[] arr) {
+        // find max
+        int max = Integer.MIN_VALUE;
+        for (int i : arr) {
+            max = max > i ? max : i;
+        }
+
+        // gather all the counts for each value
+        int[] counts = new int[max + 1];
+        for (int i : arr) {
+            counts[i]++;
+        }
+
+        // when we're dealing with ints, we can just put each value
+        // count number of times into the new array
+        int[] sorted = new int[arr.length];
+        int k = 0;
+        for (int i = 0; i < counts.length; i += 1) {
+            for (int j = 0; j < counts[i]; j += 1, k += 1) {
+                sorted[k] = i;
+            }
+        }
+
+        // however, below is a more proper, generalized implementation of
+        // counting sort that uses start position calculation
+        int[] starts = new int[max + 1];
+        int pos = 0;
+        for (int i = 0; i < starts.length; i += 1) {
+            starts[i] = pos;
+            pos += counts[i];
+        }
+
+        int[] sorted2 = new int[arr.length];
+        for (int i = 0; i < arr.length; i += 1) {
+            int item = arr[i];
+            int place = starts[item];
+            sorted2[place] = item;
+            starts[item] += 1;
+        }
+
+        // return the sorted array
+        return sorted;
+    }
+
+    /**
+     * Counting sort on the given int array, must work even with negative numbers.
+     * Note, this code does not need to work for ranges of numbers greater
+     * than 2 billion.
+     * Does not touch original array (non-destructive method).
+     *
+     * @param a int array that will be sorted
+     */
+    public static int[] betterCountingSort(int[] a) {
+        // TODO make counting sort work with arrays containing negative numbers.
+  //      return null;
+        int b[] = new int[a.length];
+        int max = a[0],min = a[0];
+        for(int i:a){
+            if(i>max){
+                max=i;
+            }
+            if(i<min){
+                min=i;
+            }
+        }//这里k的大小是要排序的数组中，元素大小的极值差+1
+        int k=max-min+1;
+        int c[]=new int[k];
+        for(int i=0;i<a.length;++i){
+            c[a[i]-min]+=1;//优化过的地方，减小了数组c的大小
+        }
+        for(int i=1;i<c.length;++i){
+            c[i]=c[i]+c[i-1];
+        }
+        for(int i=a.length-1;i>=0;--i){
+            b[--c[a[i]-min]]=a[i];//按存取的方式取出c的元素
+        }
+        return b;
+    }
+}
